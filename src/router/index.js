544f2543380c66ff6a800b40d491cd47169d15b0
Stanley Azi <azistanley17@gmail.com>
2024-06-18 16:46:19 +0100

    add: routes meta data.

--- src/router/index.js
+++ src/router/index.js
@@ -8,21 +8,37 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: {
+        title: 'Home',
+        description: "This page contains a list of public repositories and github profile card of Stanley Azi."
+      }
     },
     {
       path: "/page/:pageNumber",
       name: "page",
       component: HomeView,
+      meta: {
+        title: 'Home',
+        description: "This page contains a list of public repositories and github profile card of Stanley Azi."
+      }
     },
     {
       path: "/repos/:userLogin/:repoName/:currentPage",
       name: "repository",
       component: () => import("@/views/RepositoryDetailsView.vue"),
+      meta: {
+        title: 'Repository Details',
+        description: "This page contains the details of the active/clicked repository."
+      }
     },
     {
       path: "/:pathMatch(.*)*",
       name: "notFound",
       component: () => import("@/views/NotFoundView.vue"),
+      meta: {
+        title: 'Not Found',
+        description: "This is a 404 page."
+      }
     },
   ],
 });
